7d8b8f1d5d8be271774db49375eb3491006b423b
Andrew Ward <award@goldstar.com>
2019-06-14 13:46:07 -0700

    Replace brew install for loop with brew bundle

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -15,17 +15,7 @@ then
 fi
 
 ${BREW_COMMAND} update
-
-# Install homebrew 'essential' packages
-BREWS=(ack ctags-exuberant chruby macvim markdown proctools redis ruby-install the_silver_searcher tmux wget)
-for brew in ${BREWS[@]}; do
-  ${BREW_COMMAND} list $brew > /dev/null 2>&1
-  if [[ "$?" -eq "1" ]]; then
-    echo "Installing $brew"
-    ${BREW_COMMAND} install $brew
-  fi
-done
-
+${BREW_COMMAND} bundle install
 ${BREW_COMMAND} upgrade --all
 ${BREW_COMMAND} cleanup
 ${BREW_COMMAND} prune
