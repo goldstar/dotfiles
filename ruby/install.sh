cde6361920e622d84b96e699b2731b19a8eab09d
Rogelio J. Samour <rogelio@therubymug.com>
2019-10-29 13:39:05 -0700

    Install ruby versions

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
-if test ! $(which chruby)
-then
-  echo "  Installing chruby for you."
-  brew install chruby > /tmp/chruby_install.log
-fi
+RUBIES_TO_INSTALL=(2.6.5 2.5.5 2.5.7 2.4.3)
+DEFAULT_RUBY_VERSION=${RUBIES_TO_INSTALL[1]}
 
-if test ! $(which ruby-install)
-then
-  echo "  Installing ruby-install for you."
-  brew install ruby-install > /tmp/ruby-install_install.log
-fi
+echo "Installing rubies"
+for ruby_ver in ${RUBIES_TO_INSTALL[*]}; do
+  ls -x $HOME/.rubies | grep ${ruby_ver} 2> /dev/null
+  if [[ "$?" -eq "1" ]]; then
+    echo "Installing: ruby-${ruby_ver}"
+    ruby-install ruby ${ruby_ver}
+  else
+    echo "ruby-${ruby_ver} already installed"
+  fi
+done
+
+echo "Setting default ruby to ${DEFAULT_RUBY_VERSION}"
+echo ${DEFAULT_RUBY_VERSION} > $HOME/.ruby-version
