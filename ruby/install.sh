4caf19c174702a9892fa7102160176c1209ff59f
Rogelio J. Samour <rogelio@therubymug.com>
2022-01-19 15:09:20 -0800

    Update default homebrew packages needed for dev

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-RUBIES_TO_INSTALL=(2.6.5 2.5.5 2.5.7 2.4.3)
+RUBIES_TO_INSTALL=(2.6.8 2.6.9)
 DEFAULT_RUBY_VERSION=${RUBIES_TO_INSTALL[1]}
 
 echo "Installing rubies"
@@ -13,6 +13,12 @@ for ruby_ver in ${RUBIES_TO_INSTALL[*]}; do
     echo "ruby-${ruby_ver} already installed"
   fi
 done
+
+echo "To compile mysql2, you may need to run:"
+echo
+echo 'bundle config --local build.mysql2 "--with-ldflags=-L/usr/local/opt/openssl/lib --with-cppflags=-I/usr/local/opt/openssl/include"'
+echo "gem install mysql2 -v '0.4.10'"
+echo
 
 echo "Setting default ruby to ${DEFAULT_RUBY_VERSION}"
 echo ${DEFAULT_RUBY_VERSION} > $HOME/.ruby-version
