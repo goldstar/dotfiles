006673d95315898911fd96b99fbe4ba3af16dc2f
Christopher Sessions <15719657+silhouette88@users.noreply.github.com>
2022-11-21 16:51:41 -0800

    Update

--- vim/install.sh
+++ vim/install.sh
@@ -9,7 +9,7 @@ get_bundle() {
     cd "$2"
     git pull --rebase
   else
-    git clone "git://github.com/$1/$2.git"
+    git clone "https://github.com/$1/$2.git"
   fi
   )
 }
@@ -22,6 +22,7 @@ get_bundle jgdavey tslime.vim
 get_bundle jgdavey vim-blockle
 get_bundle jgdavey vim-turbux
 get_bundle junegunn fzf
+get_bundle junegunn fzf.vim
 get_bundle kchmck vim-coffee-script
 get_bundle mileszs ack.vim
 get_bundle pangloss vim-javascript
@@ -32,7 +33,7 @@ get_bundle tomasr molokai
 get_bundle tpope vim-abolish
 get_bundle tpope vim-bundler
 get_bundle tpope vim-commentary
-get_bundle  fatih vim-go
+get_bundle fatih vim-go
 get_bundle tpope vim-cucumber
 get_bundle tpope vim-endwise
 get_bundle tpope vim-eunuch
@@ -56,5 +57,11 @@ get_bundle tpope vim-tbone
 get_bundle tpope vim-unimpaired
 get_bundle tpope vim-vividchalk
 get_bundle vim-scripts bufkill.vim
+get_bundle ekalinin Dockerfile.vim
+get_bundle christoomey vim-tmux-navigator
+get_bundle ngmy vim-rubocop
+get_bundle mbbill undotree
+get_bundle vimwiki vimwiki
+get_bundle michal-h21 vim-zettel
 
 vim -c 'call pathogen#helptags()|q'
